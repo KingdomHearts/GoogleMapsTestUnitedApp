fb08aff01eb73a29b122ed1ce0c4bddf82fa9371
MichaelBossink <MichaelBossink@LYC-5CD3257V23>
2024-08-14 12:10:14 +0200

    init

---
+++ GoogleMapsTestUnitedApp/Program.cs
@@ -0,0 +1,42 @@
+using GoogleMapsComponents;
+using GoogleMapsTestUnitedApp.Client.Pages;
+using GoogleMapsTestUnitedApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBlazorGoogleMaps("INSERT_API_KEY");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+
+app.MapBlazorHub();
+//app.MapFallbackToPage("/_Host");
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(GoogleMapsTestUnitedApp.Client._Imports).Assembly);
+
+app.Run();
